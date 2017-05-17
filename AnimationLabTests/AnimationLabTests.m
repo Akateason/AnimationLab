ba80ea74ad4cc89cf479abff0eeb8c1956726dcb
teason23 <akateason23@gmail.com>
2017-05-17 15:08:18 +0800

    Initial Commit

---
+++ AnimationLabTests/AnimationLabTests.m
@@ -0,0 +1,39 @@
+//
+//  AnimationLabTests.m
+//  AnimationLabTests
+//
+//  Created by teason23 on 2017/5/17.
+//  Copyright © 2017年 teaason. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AnimationLabTests : XCTestCase
+
+@end
+
+@implementation AnimationLabTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
