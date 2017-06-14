156ab0925284c47828462a011977f25bbae9300c
tianchen.xie <tianchen.xie@phicomm.com>
2017-06-14 13:45:29 +0800

    cmt 1za1 transition animation .

--- AnimationLab/AnimationViewController.m
+++ AnimationLab/AnimationViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "AnimationViewController.h"
+#import "HomeCell.h"
+#import <objc/runtime.h>
 
-@interface AnimationViewController ()
+@interface AnimationViewController () <UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *table;
 
 @end
 
@@ -17,12 +20,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.table.dataSource   = self ;
+    self.table.delegate     = self ;
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10 ;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    HomeCell *cell = [tableView dequeueReusableCellWithIdentifier:@"HomeCell"] ;
+    cell.textLabel.text = [NSString stringWithFormat:@"ZA%dViewController",(int)indexPath.row+1] ;
+    return cell ;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 50 ;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    HomeCell *cell = (HomeCell *)[tableView cellForRowAtIndexPath:indexPath] ;
+    NSString *title = cell.textLabel.text ;
+    Class cls = objc_getRequiredClass([title UTF8String]) ;
+    UIViewController *ctrller = [[cls alloc] init] ;
+    ctrller.title = title ;
+    [self.navigationController pushViewController:ctrller animated:YES] ;
+}
+
+
 
 /*
 #pragma mark - Navigation
